fcc6da86a1f8a5c40679d8acf51215db356dc361
manuel <mduvens@gmail.com>
2020-10-27 02:21:49 +0000

    ver1

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.get('/workouts', function(req,res){
 app.get('/app', function(req,res){
     res.sendFile(__dirname + '/app.html')
 })
+app.get('/.well-known/pki-validation/F6EF37DF1D5F507A66EE9110E3DC693B.txt', function(req,res){
+    res.sendFile(__dirname + '/F6EF37DF1D5F507A66EE9110E3DC693B.txt')
+})
 app.get('/about', function(req,res){
     res.sendFile(__dirname + '/about.html')
 })
